adacdca261b93471a52c51e001cb41083cfb75a0
Nancy Barragan <nan.ba.he@gmail.com>
2018-10-03 15:43:57 -0500

    agregar login y formulario

---
+++ resources/views/formulario.blade.php
@@ -0,0 +1,157 @@
+@extends('plantillas.menu')
+@section('title','Formulario')
+@section('nombre_usuario','Marvin Eliosa')
+@section('tittle_page','Descripción de Puestos')
+
+@section('content')
+
+	<div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <!--<div class="x_title">
+                    <h2>Seccion 1 de la página</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>-->
+                  <div class="x_content">
+<!--Agregar el formulario -->
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2><i class="fa fa-bars"></i> "Puesto" <small>Descripcion de puestos</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+
+                    <div class="col-xs-3">
+                      <!-- required for floating -->
+                      <!-- Nav tabs -->
+                      <ul class="nav nav-tabs tabs-left">
+                        <li class="active"><a href="#InfoGe" data-toggle="tab">Información General</a>
+                        </li>
+                        <li><a href="#Actividad" data-toggle="tab">Actividades</a>
+                        </li>
+                        <li><a href="#Relacion" data-toggle="tab">Relaciones Criticas</a>
+                        </li>
+                        <li><a href="#Perfil" data-toggle="tab">Perfil del Puesto</a>
+                        </li>
+                        <li><a href="#Competencia" data-toggle="tab">Competencias</a>
+                        </li>
+                        <li><a href="#Distribucion" data-toggle="tab">Lista de Distribución</a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div class="col-xs-9">
+                      <!-- Tab panes -->
+                      <div class="tab-content">
+                        <div class="tab-pane active" id="InfoGe">
+                          <p class="lead">Informacion General y Porposito General del puesto</p>
+  <!--Formulario de informacion General-->
+                          <div class="x_panel">
+                          <div class="x_content">
+                          <br />
+                        <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">First Name <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Last Name <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Middle Name / Initial</label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <div id="gender" class="btn-group" data-toggle="buttons">
+                                <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                  <input type="radio" name="gender" value="male"> &nbsp; Male &nbsp;
+                                </label>
+                                <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                  <input type="radio" name="gender" value="female"> Female
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Date Of Birth <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input id="birthday" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                            </div>
+                          </div>
+                          <div class="ln_solid"></div>
+                          <div class="form-group">
+                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                              <button class="btn btn-primary" type="button">Cancel</button>
+                          <button class="btn btn-primary" type="reset">Reset</button>
+                              <button type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                          </div>
+                    </form>
+                  </div>
+                </div>
+                          <!--Fin del Formulario de información General-->
+                         
+                        </div>
+                        <div class="tab-pane" id="Actividad">Profile Tab.</div>
+                        <div class="tab-pane" id="Relacion">Messages Tab.</div>
+                        <div class="tab-pane" id="Perfil">Settings Tab.</div>
+                        <div class="tab-pane" id="Competencia">Settings Tab.</div>
+                        <div class="tab-pane" id="Distribucion">Settings Tab.</div>
+                      </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                  </div>
+                </div>
+              </div>
+
+
+                  </div>
+                </div>
+              </div>
+            </div>
+@endsection
